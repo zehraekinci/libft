618fa566b6c54af8dde092870f8381f012f65ae8
zehra ekinci <zehraekinci2109@gmail.com>
2024-10-29 16:57:46 +0300

    modified:   libft.h

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,8 +6,23 @@
 /*   By: zeekinci <zeekinci@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 09:45:41 by zeekinci          #+#    #+#             */
-/*   Updated: 2024/10/26 09:45:41 by zeekinci         ###   ########.fr       */
+/*   Updated: 2024/10/29 12:15:09 by zeekinci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+#include <stdio.h>
+
+int ft_isalnum(int c)
+{
+ 
+    return(ft_isalpha(c) || ft_isalpha(c));
+}
+
+int main()
+{
+    int ft_isalpha(int c);
+    int ft_isdigit(int c);
+    printf("%d\n", ft_isalnum('a'));
+}
--- libft.h
+++ libft.h
@@ -6,14 +6,16 @@
 /*   By: zeekinci <zeekinci@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 09:46:41 by zeekinci          #+#    #+#             */
-/*   Updated: 2024/10/26 09:46:41 by zeekinci         ###   ########.fr       */
+/*   Updated: 2024/10/29 11:59:30 by zeekinci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT.H
+#ifndef LIBFT_H
 
-#define LIBFT.H
+#define LIBFT_H
 
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
 
-
-#endif libft.h
+#endif
